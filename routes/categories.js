13d47218ebedcff2257b81018024f64f14fc76c2
Abhansh Giri <abhanshgiri21@gmail.com>
2017-08-09 20:58:29 +0530

    first commit

---
+++ routes/categories.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var cassandra = require('cassandra-driver');
+var client = new cassandra.Client({contactPoints:['127.0.0.1']});
+client.connect(function(err, result){
+  console.log('Cassandra Connect'); 
+});
+
+
+
+router.get('/add', function(req, res, next) {
+  res.render('addcategory', {title:'addcat'});
+});
+
+router.post('/add', function(req, res, next) {
+    var cat_id = cassandra.types.uuid();
+    var query = "INSERT INTO findadoc.categories(cat_id, name) VALUES(?,?)";
+
+    client.execute(query, [cat_id, req.body.name],{prepare:true}, function(err, result){
+      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&');
+      console.log(result);
+      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&');
+      console.log(req.body);
+      if(err){
+        res.status(404).send({msg:err});
+      }else{
+        req.flash('success', 'Category Added');
+        res.location('/doctors');
+        res.redirect('/doctors');
+      }
+    } );
+});
+
+module.exports = router;
